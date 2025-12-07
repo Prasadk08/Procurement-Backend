f82c8b6af420c49abbf162fad694b15e0254028e
Prasadk08 <prasadkshirsagar1199@gmail.com>
2025-12-07 20:31:31 +0530

    gemini

--- Ai-Agent/gemini.js
+++ Ai-Agent/gemini.js
@@ -1,8 +1,11 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const model = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash-lite",
-  apiKey: "AIzaSyDfPl1sTY-Vpq7Qbivn6bq6-eW-wJacYgk",
+  apiKey: process.env.GEMINI_KEY,
 });
 
 
